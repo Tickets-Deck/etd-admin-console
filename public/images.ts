85b1e4194b88515965242f5c4a6d9763a5557080
Simlex <simlexafol@gmail.com>
2024-06-30 20:04:39 +0100

    fix: add images constant

---
+++ public/images.ts
@@ -0,0 +1,9 @@
+import logoWhite from "./images/etd-logo-white.png";
+import logoPurple from "./images/etd-logo.png";
+import avatar from "./images/user_avatar.png";
+
+export default {
+  logoWhite,
+  logoPurple,
+  avatar,
+};
