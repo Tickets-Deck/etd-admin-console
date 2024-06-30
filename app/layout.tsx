c29fd6a7cebb52a3101e83f4c78d33600b4ebd06
Simlex <simlexafol@gmail.com>
2024-06-30 19:59:39 +0100

    fix: add layout

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.scss";
+import Layout from "./shared/Layout";
+import { getServerSession } from "next-auth";
+import { authOptions } from '@/auth';
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Ticketsdeck Events Admin Console",
+    description: "The admin console for Ticketsdeck Events.",
+};
+
+export default async function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+
+    const session = await getServerSession(authOptions);
+
+    return (
+        <html lang="en">
+            <body className="bg-white">
+                <Layout
+                    children={children}
+                    session={session}
+                />
+            </body>
+        </html>
+    );
+}
