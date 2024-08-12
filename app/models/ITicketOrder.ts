f083eeade2fbdcc0d24cefc709bb9d4fd6e50801
Simlex <simlexafol@gmail.com>
2024-08-12 10:49:05 +0100

    refactor: refactor ITicketOrder interface and add PaymentStatus enum

--- app/models/ITicketOrder.ts
+++ app/models/ITicketOrder.ts
@@ -1,74 +1,19 @@
-import { EventVisibility } from "../enums/IEventVisibility";
 import { OrderStatus } from "../enums/IOrderStatus";
+import { PaymentStatus } from "../enums/IPaymentStatus";
+import { Event } from "./IEvent";
 
-export interface SingleTicketOrderRequest {
-  ticketId: string;
-  price: number;
-  associatedEmail: string;
-  contactEmail: string;
-}
-
-export interface TicketOrderRequest {
-  userId: string;
-  eventId: string;
-  tickets: SingleTicketOrderRequest[];
-  contactEmail: string;
-}
-
-//#region Responses
-
-export interface TicketEventOrderResponse {
+export interface TicketOrder {
     id: string;
+    userId: string;
     eventId: string;
-    publisherId: string;
-    title: string;
-    description: string;
-    locationId: null;
-    venue: string;
-    date: string;
-    time: string;
-    category: string;
-    visibility: EventVisibility;
-    mainImageUrl: string;
-    mainImageId: string;
-    currency: string;
-    purchaseStartDate: string;
-    purchaseEndDate: string;
-    allowedGuestType: string;
-    bookmarksCount: number;
-    favoritesCount: number;
-    ticketOrdersCount: number;
+    quantity: number;
+    totalPrice: string;
+    contactEmail: string;
+    orderId: string;
+    orderStatus: OrderStatus;
+    paymentStatus: PaymentStatus;
+    paymentId: string;
     createdAt: string;
     updatedAt: string;
-}
-
-export interface TicketOrderResponse {
-  id: string;
-  eventId: string;
-  name: string;
-  price: string;
-  quantity: number;
-  remainingTickets: number;
-  numberOfUsers: number;
-  description: null;
-  ticketOrdersCount: number;
-  createdAt: string;
-  updatedAt: string;
-  event: TicketEventOrderResponse;
-}
-
-export interface UserTicketOrder {
-  id: string;
-  ticketId: string;
-  orderId: string;
-  associatedEmail: string;
-  contactEmail: string;
-  price: string;
-  orderStatus: OrderStatus;
-  paymentId: string;
-  createdAt: string;
-  updatedAt: string;
-  ticket: TicketOrderResponse;
-}
-
-//#endregion
+    event: Event;
+  }
