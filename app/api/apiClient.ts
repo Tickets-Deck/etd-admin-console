97ad37bd5a07901ac10e3bc742edb917ddbe67f3
Simlex <simlexafol@gmail.com>
2024-08-12 11:08:48 +0100

    feat: update API base URL to live environment

--- app/api/apiClient.ts
+++ app/api/apiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ApiRoutes } from "./apiRoutes";
 
 export const API = axios.create({
-  baseURL: ApiRoutes.BASE_URL_TEST,
+  baseURL: ApiRoutes.BASE_URL_LIVE,
 });
 
 export function useFetchDashboardInfo() {
