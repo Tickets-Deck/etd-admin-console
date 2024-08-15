368f12f4eac67af53c0218d6d7a4bc4616528376
Simlex <simlexafol@gmail.com>
2024-08-15 23:07:58 +0100

    feat: add FullPageLoader component to handle loading state

--- app/homepage/Homepage.tsx
+++ app/homepage/Homepage.tsx
@@ -9,6 +9,7 @@ import { catchError } from "../constants/catchError";
 import Kpi from "./Kpi";
 import RecentTransactions from "./RecentTransactions";
 import { IRecentTransactions } from "../models/IRecentTransactions";
+import { FullPageLoader } from "../components/Loader/ComponentLoader";
 
 type HomepageProps = {
 }
@@ -23,7 +24,7 @@ const Homepage: FunctionComponent<HomepageProps> = (): ReactElement => {
 
     const [isLoading, setIsLoading] = useState(true);
     const [dashboardInfo, setDashboardInfo] = useState<DashboardInfoResponse>();
-    const [recentTransactions, setRecentTransactions] = useState<IRecentTransactions[]>([]);
+    const [recentTransactions, setRecentTransactions] = useState<IRecentTransactions[]>();
 
     async function handleFetchDashboardInfo() {
 
@@ -61,31 +62,37 @@ const Homepage: FunctionComponent<HomepageProps> = (): ReactElement => {
 
     return (
         <main className="bg-light-grey flex min-h-screen flex-col items-center justify-between p-4 pt-[calc(136px_+_24px)]">
-            <div className="w-full flex flex-col gap-4 mb-8 md:flex-row">
-                <Kpi
-                    path={ApplicationRoutes.Users}
-                    title="Total Users"
-                    icon={<Icons.UserOutline className="w-6 h-6" />}
-                    value={dashboardInfo?.totalUsers.toString() || "0"}
-                />
-                <Kpi
-                    path={ApplicationRoutes.Payments}
-                    title="Total Payments"
-                    icon={<Icons.PaymentOutline className="w-6 h-6" />}
-                    value={dashboardInfo?.totalRevenue.toLocaleString() || "0"}
-                    priceKpi
-                />
-                <Kpi
-                    path={ApplicationRoutes.Events}
-                    title="Total Events"
-                    icon={<Icons.EventsOutline className="w-6 h-6" />}
-                    value={dashboardInfo?.totalEvents.toString() || "0"}
-                />
-            </div>
+            {
+                recentTransactions ?
+                    <>
+                        <div className="w-full flex flex-col gap-4 mb-8 md:flex-row">
+                            <Kpi
+                                path={ApplicationRoutes.Users}
+                                title="Total Users"
+                                icon={<Icons.UserOutline className="w-6 h-6" />}
+                                value={dashboardInfo?.totalUsers.toString() || "0"}
+                            />
+                            <Kpi
+                                path={ApplicationRoutes.Payments}
+                                title="Total Payments"
+                                icon={<Icons.PaymentOutline className="w-6 h-6" />}
+                                value={dashboardInfo?.totalRevenue.toLocaleString() || "0"}
+                                priceKpi
+                            />
+                            <Kpi
+                                path={ApplicationRoutes.Events}
+                                title="Total Events"
+                                icon={<Icons.EventsOutline className="w-6 h-6" />}
+                                value={dashboardInfo?.totalEvents.toString() || "0"}
+                            />
+                        </div>
 
-            <RecentTransactions
-                recentTransactions={recentTransactions}
-            />
+                        <RecentTransactions
+                            recentTransactions={recentTransactions}
+                        />
+                    </> :
+                    <FullPageLoader />
+            }
         </main>
     );
 }
