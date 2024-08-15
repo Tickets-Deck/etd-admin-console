e25d9b1bb3893abc7d57ee933a6e8c9f1f643e3c
Simlex <simlexafol@gmail.com>
2024-08-15 23:08:37 +0100

    refactor: refactor RecentTransactions component

--- app/homepage/RecentTransactions.tsx
+++ app/homepage/RecentTransactions.tsx
@@ -11,6 +11,7 @@ type Props = {
 }
 
 const RecentTransactions = ({ recentTransactions }: Props) => {
+    
     return (
         <div className="w-full flex flex-col gap-4 bg-white p-5 rounded-2xl">
             <div className="flex flex-col items-start md:flex-row md:justify-between">
@@ -29,7 +30,7 @@ const RecentTransactions = ({ recentTransactions }: Props) => {
                                         <Icons.Credit className="w-8 h-8 stroke-dark-grey" />
                                     </span>
                                     <div className="flex flex-col">
-                                        <h4 className="font-normal text-lg text-dark-grey leading-6">{transaction.contactEmail}</h4>
+                                        <h4 className="font-normal text-base text-dark-grey leading-6">{`${transaction.contactEmail} ordered ${transaction.quantity} ${transaction.quantity > 1 ? "tickets" : "ticket"}`}</h4>
                                         <div className="flex flex-row items-center gap-2">
                                             <p className="text-sm text-dark-grey/50">{moment(transaction.createdAt).format("MMM D, YYYY")}</p>
                                             <span className="text-dark-grey">|</span>
@@ -38,14 +39,17 @@ const RecentTransactions = ({ recentTransactions }: Props) => {
                                     </div>
                                 </div>
                                 <div className="flex flex-row items-center justify-between">
-                                    <span className="font-semibold text-xl text-dark-grey font-MonaSansWide">
-                                        {
-                                            Number(transaction.totalPrice) > 0 ?
-                                                <>&#8358;{Number(transaction.totalPrice).toLocaleString()}</> :
-                                                <>Free</>
-                                        }
-                                    </span>
-                                    <Button className="p-2 px-5 !bg-gray-100 !text-dark-grey/80 text-sm rounded-full hover:opacity-55">View transaction</Button>
+                                    <div className='flex flex-col md:flex-row md:items-center md:gap-3'>
+                                        <p className='max-w-[220px] md:max-w-none overflow-hidden whitespace-nowrap text-ellipsis'>Event: {transaction.event.title}</p>
+                                        <span className="font-semibold text-xl text-dark-grey font-MonaSansWide">
+                                            {
+                                                Number(transaction.totalPrice) > 0 ?
+                                                    <>&#8358;{Number(transaction.totalPrice).toLocaleString()}</> :
+                                                    <>Free</>
+                                            }
+                                        </span>
+                                    </div>
+                                    <Button className="!p-2 !px-5 !bg-gray-100 !text-dark-grey/80 text-sm rounded-full hover:opacity-55">View</Button>
                                 </div>
                             </li>
                         )
