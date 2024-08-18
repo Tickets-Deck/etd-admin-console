f0193db91952235dbb21142840773f72cb2fc388
Simlex <simlexafol@gmail.com>
2024-08-18 13:24:36 +0100

    feat: update model

--- app/models/ITicketOrder.ts
+++ app/models/ITicketOrder.ts
@@ -1,19 +1,60 @@
 import { OrderStatus } from "../enums/IOrderStatus";
+import { PaymentServiceProvider } from "../enums/IPaymentServiceProvider";
 import { PaymentStatus } from "../enums/IPaymentStatus";
-import { Event } from "./IEvent";
 
-export interface TicketOrder {
-    id: string;
-    userId: string;
-    eventId: string;
-    quantity: number;
-    totalPrice: string;
-    contactEmail: string;
-    orderId: string;
-    orderStatus: OrderStatus;
-    paymentStatus: PaymentStatus;
-    paymentId: string;
-    createdAt: string;
-    updatedAt: string;
-    event: Event;
-  }
+interface TicketOrderPayment {
+  paymentServiceProvider: PaymentServiceProvider;
+}
+
+interface TicketOrderEvent {
+  title: string;
+}
+
+interface SingleTicketOrderUser {
+  firstName: string;
+  lastName: string;
+  phone: string | null;
+  email: string;
+}
+
+interface SingleTicketOrderOrderedTicketTicket {
+    name: string;
+}
+
+interface SingleTicketOrderOrderedTicket {
+  ticket: SingleTicketOrderOrderedTicketTicket
+}
+
+export interface FetchSingleTicketOrderRequest {
+  userId: string;
+  orderId: string;
+}
+
+export interface TicketOrderResponse {
+  id: string;
+  contactEmail: string;
+  orderId: string;
+  totalPrice: string;
+  createdAt: string;
+  quantity: number;
+  paymentStatus: PaymentStatus;
+  payments: TicketOrderPayment[];
+  event: TicketOrderEvent;
+}
+
+export interface SingleTicketOrder {
+  id: string;
+  userId: string;
+  event: TicketOrderEvent;
+  quantity: number;
+  totalPrice: string;
+  contactEmail: string;
+  orderId: string;
+  orderStatus: OrderStatus;
+  paymentStatus: PaymentStatus;
+  paymentId: string;
+  createdAt: string;
+  updatedAt: string;
+  tickets: SingleTicketOrderOrderedTicket[];
+  user: SingleTicketOrderUser | null;
+}
