483f065552f053031e62b94abddedee53445b7ec
Simlex <simlexafol@gmail.com>
2024-09-29 22:59:02 +0100

    refactor: Update PaymentPage component
    - Remove unused imports and code
    - Add button component for payment confirmation
    - Implement payment verification functionality
    - Display toast messages for payment status
    
    Closes #123

--- app/payments/PaymentPage.tsx
+++ app/payments/PaymentPage.tsx
@@ -1,8 +1,6 @@
 "use client"
 import React, { useEffect, useState } from 'react'
 import { styles } from '../styles/styles'
-import Link from "next/link";
-import { Icons } from '../components/ui/icons';
 import Table from '../components/ui/table';
 import { useFetchPayments } from '../api/apiClient';
 import { useSession } from "next-auth/react";
@@ -10,8 +8,10 @@ import { catchError } from '../constants/catchError';
 import { Payment } from '../models/IPayment';
 import moment from 'moment';
 import { serializer } from '../constants/serializer';
-import { PaymentStatus } from '../enums/IPaymentStatus';
 import { NairaPrice } from '../constants/priceFormatter';
+import { ButtonLoader } from '../components/Loader/ComponentLoader';
+import { toast } from "sonner";
+import Button from '../components/ui/button';
 
 type Props = {}
 
@@ -23,6 +23,8 @@ const PaymentPage = (props: Props) => {
     const user = session?.user;
 
     const [isLoading, setIsLoading] = useState(true);
+    const [isVerifyingPayment, setIsVerifyingPayment] = useState(false);
+    const [selectedTrxref, setSelectedTrxref] = useState<string>();
     const [payments, setPayments] = useState<Payment[]>([]);
 
     async function handleFetchPayments() {
@@ -38,6 +40,38 @@ const PaymentPage = (props: Props) => {
                 setIsLoading(false);
             });
     };
+
+    async function handleVerifyPayment(trxref: string) {
+        setIsVerifyingPayment(true);
+
+        const response = await fetch(`/api/payments/verify?trxref=${trxref}`);
+        const data = await response.json();
+        console.log("Payment verification data", data);
+
+        if (data?.message == "Payment successfully processed." || data?.status == 'success') {
+            toast.success("Payment was successful.");
+            postVerifyPayment();
+            return;
+        }
+        if (data?.status == 'abandoned' || data?.status == 'ongoing' || data?.status == 'pending') {
+            toast.error("Payment still pending.");
+            postVerifyPayment();
+            return;
+        }
+        if (data?.status == 'failed') {
+            toast.error("Payment failed.");
+            postVerifyPayment();
+            return;
+        }
+
+        toast.error("Payment could not be verified.");
+        postVerifyPayment();
+
+        function postVerifyPayment() {
+            setIsVerifyingPayment(false);
+            handleFetchPayments();
+        }
+    }
 
     useEffect(() => {
         if (!user) return;
@@ -77,15 +111,26 @@ const PaymentPage = (props: Props) => {
                                     <span className="ml-2 text-sm w-48 text-wrap">{payment.ticketOrder.contactEmail}</span>
                                 </div>,
                                 <span className="text-[#666666] text-left">{payment.ticketOrder.event.title}</span>,
-                                <span className="text-[#666666]">{NairaPrice.format(Number(payment.amount))}</span>, 
+                                <span className="text-[#666666]">{NairaPrice.format(Number(payment.amount))}</span>,
                                 <span className="text-[#666666] whitespace-nowrap">{moment(payment.createdAt).format("MMM D, YYYY | hh:mma")}</span>,
                                 <span className="text-[#666666]">{payment.ticketOrder.quantity}</span>,
                                 <span className="text-[#666666]">{payment.paymentServiceProvider}</span>,
                                 <span className={styles.getStatusStyle(payment.paymentStatus)}>{serializer.paymentStatus(payment.paymentStatus)}</span>,
                                 <div className='flex flex-row gap-1'>
-                                    <button className="p-2 px-4 bg-gray-200 text-dark-grey/80 text-xs rounded-full hover:opacity-55">
-                                        Confirm
-                                    </button>
+                                    {
+                                        // payment.paymentStatus === PaymentStatus.Pending &&
+                                        <Button
+                                            disabled={isVerifyingPayment}
+                                            isLoading={isVerifyingPayment && payment.paymentReference == selectedTrxref}
+                                            onClick={() => {
+                                                setSelectedTrxref(payment.paymentReference);
+                                                handleVerifyPayment(payment.paymentReference)
+                                            }}
+                                            className="!p-2 !px-4 !bg-gray-200 !text-dark-grey/80 !text-xs !rounded-full !hover:opacity-55 transition-all">
+                                            Confirm
+                                            {/* {isLoading && <ButtonLoader />} */}
+                                        </Button>
+                                    }
                                     <button className="p-1 text-dark-grey/80 text-xs hover:opacity-55">
                                         View details
                                     </button>
