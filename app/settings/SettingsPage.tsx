f3adcab6fe4424302e4ef671b2610cac86dca999
Simlex <simlexafol@gmail.com>
2025-02-11 17:36:07 +0100

    feat: implement pagination and enhance fetching logic

--- app/settings/SettingsPage.tsx
+++ app/settings/SettingsPage.tsx
@@ -1,23 +1,13 @@
 "use client"
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { styles } from "../styles/styles";
-import Button from '../components/ui/button';
-import { useCreateCouponCode, useCreateTransactionFee, useDeleteCouponCode, useDeleteTransactionFee, useFetchCouponCodes, useFetchTransactionFees, useFetchUsers } from '../api/apiClient';
-import { User } from '../models/IUser';
 import { useSession } from "next-auth/react";
-import { TransactionFeeRequest, TransactionFeeResponse } from '../models/ITransactionFee';
-import TransactionFeeCreationModal from '../components/Modal/TransactionFeeCreationModal';
-import { catchError } from '../constants/catchError';
 import TransactionFeeSection from '../components/settings/TransactionFeeSection';
-import { CouponCodeRequest, CouponCodeResponse } from '../models/ICoupon';
-import moment from 'moment';
 import CouponCodeSection from '../components/settings/CouponCodeSection';
 
-type Props = {}
+export const SettingsPage = () => {
 
-export const SettingsPage = (props: Props) => {
-
-    const { data: session, status } = useSession();
+    const { data: session } = useSession();
     
     return (
         <main className={styles.mainPageStyle}>
