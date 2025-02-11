1b58e03ef54175e915d2fcaecd0004524ed8ab72
Simlex <simlexafol@gmail.com>
2025-02-11 17:34:36 +0100

    feat: Enhance ApplicationContext to fetch and manage admin user data

--- app/contexts/ApplicationContext.tsx
+++ app/contexts/ApplicationContext.tsx
@@ -1,8 +1,14 @@
-import { Dispatch, FunctionComponent, ReactNode, SetStateAction, createContext, useEffect, useState } from "react";
+import { Dispatch, FunctionComponent, ReactNode, SetStateAction, createContext, useContext, useEffect, useState } from "react";
 import { useOnline } from "../hooks/useOnline";
+import { AdminUser } from "../models/IAdminUser";
+import { useFetchAdminUser } from "../api/apiClient";
+import { useSession } from "next-auth/react";
+import { catchError } from "../constants/catchError";
 
 // Define the type for the context data
 export type ApplicationContextData = {
+    adminUser: AdminUser | null;
+    handleFetchAdminUser: () => void;
     offlineStatusModalVisibility: boolean;
     hideOfflineStatusModalVisibility: Dispatch<SetStateAction<boolean>>
 };
@@ -17,9 +23,37 @@ type AppProviderProps = {
 
 const AppProvider: FunctionComponent<AppProviderProps> = ({ children }) => {
 
+    const fetchAdminUser = useFetchAdminUser();
+    const { data: session, status } = useSession();
+    const user = session?.user;
+
+    const [adminUser, setAdminUser] = useState<AdminUser | null>(null);
     const [offlineStatusModalVisibility, setOfflineStatusModalVisibility] = useState(false);
+    const [isFetchingAdminUser, setIsFetchingAdminUser] = useState(false);
 
     const isOnline = useOnline();
+
+    const handleFetchAdminUser = async () => {
+        // Show loader
+        setIsFetchingAdminUser(true);
+
+        // Fetch admin user
+        await fetchAdminUser(user?.id as string, user?.token as string)
+            .then((response) => {
+                setAdminUser(response.data);
+            })
+            .catch((error) => {
+                catchError(error);
+            })
+            .finally(() => {
+                setIsFetchingAdminUser(false);
+            });
+    }
+
+    useEffect(() => {
+        if (!user) return;
+        handleFetchAdminUser();
+    }, [user]);
 
     useEffect(() => {
         if (!isOnline) {
@@ -31,6 +65,8 @@ const AppProvider: FunctionComponent<AppProviderProps> = ({ children }) => {
 
     // Define the values you want to share
     const sharedData: ApplicationContextData = {
+        adminUser,
+        handleFetchAdminUser,
         offlineStatusModalVisibility,
         hideOfflineStatusModalVisibility: setOfflineStatusModalVisibility
     };
@@ -43,3 +79,11 @@ const AppProvider: FunctionComponent<AppProviderProps> = ({ children }) => {
 };
 
 export { AppProvider, ApplicationContext };
+
+export const useApplicationContext = () => {
+    const context = useContext(ApplicationContext);
+    if (context === undefined) {
+        throw new Error("useApplicationContext must be used within a AppProvider");
+    }
+    return context;
+}
